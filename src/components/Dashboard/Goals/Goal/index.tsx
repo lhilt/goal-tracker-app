02c0a2e9a8b1667d33c49df7dc5cd70abb0943a0
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2020-01-26 21:09:40 -0800

    Dispatch action to toggle "completed" on Goal component

--- src/components/Dashboard/Goals/Goal/index.tsx
+++ src/components/Dashboard/Goals/Goal/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Dispatch as ReduxDispatch } from 'redux';
 import { TGoal, ReduxState } from '../../../../types';
+import { TGoalAction, toggleCompleted } from '../../../../actions';
 import './Goal.scss';
 
 interface OwnProps {
@@ -11,10 +13,17 @@ interface StoreProps {
   goal: TGoal;
 }
 
-const Goal: React.FC<StoreProps> = ({ goal }) => {
+type Dispatch = ReduxDispatch<TGoalAction>;
+interface DispatchProps {
+  onClick: () => void;
+}
+
+type Props = StoreProps & DispatchProps;
+
+const Goal: React.FC<Props> = ({ goal, onClick }) => {
   return (
     <div className="goal">
-      <div className="checkbox">{goal.completed ? 'X' : ''}</div>
+      <div className="checkbox" onClick={onClick}>{goal.completed ? 'X' : ''}</div>
       <div className="goal-text">{goal.text}</div>
     </div>
   );
@@ -26,5 +35,15 @@ const mapStateToProps = (state: ReduxState, ownProps: OwnProps) => {
   });
 };
 
+const mapDispatchToProps = (dispatch: Dispatch, ownProps: OwnProps) => {
+  const { goalId } = ownProps;
+  return ({
+    onClick: () => dispatch(toggleCompleted(goalId))
+  });
+}
+
 export default
-  connect<StoreProps, {}, OwnProps, ReduxState>(mapStateToProps)(Goal);
+  connect<StoreProps, DispatchProps, OwnProps, ReduxState>(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Goal);
